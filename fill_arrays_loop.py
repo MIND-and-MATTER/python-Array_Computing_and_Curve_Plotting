1d0eddba21a76a6ea8fc391fda62f13defeb46a7
lucca001 <>
2021-04-08 20:43:40 +0000

    fill_arrays_loop - complete

--- fill_arrays_loop.py
+++ fill_arrays_loop.py
@@ -1,2 +1,22 @@
 # Fill arrays; loop version
 
+import numpy as np
+import math as m
+
+
+def h(x):
+  num = m.exp(-(x*x)/2)
+  den = m.sqrt(2*m.pi)
+  h = num / den
+  return h
+
+xlist = np.linspace(-4, 4, 41)
+x = np.empty([len(xlist)])
+y = np.empty([len(xlist)])
+
+for i in range(0, 41):
+  x[i] = xlist[i]
+  y[i] = h(xlist[i])
+  
+for i, j in zip(x,y):
+  print(i, j)
--- fill_arrays_vectorized.py
+++ fill_arrays_vectorized.py
@@ -1,2 +1,15 @@
 # Fill arrays; vectorized version
 
+import numpy as np
+
+def h(x):
+  num = np.exp(-(x*x)/2)
+  den = np.sqrt(2*np.pi)
+  h = num / den
+  return h
+
+xlist = np.linspace(-4, 4, 41)
+ylist = h(xlist)
+
+
+print(xlist, ylist)
