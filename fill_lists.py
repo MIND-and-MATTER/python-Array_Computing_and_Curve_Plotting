45c49df4ec898b7c39581cb2525ba44c7ede7fd7
lucca001 <>
2021-04-08 19:48:52 +0000

    fill_lists - complete

--- fill_lists.py
+++ fill_lists.py
@@ -1,2 +1,20 @@
 # Fill lists with function values
 
+import numpy as np
+import math as m
+
+def h(x):
+  num = m.exp(-(x*x)/2)
+  den = m.sqrt(2*m.pi)
+  h = num / den
+  return h
+
+
+xlist = np.linspace(-4, 4, 41)
+hlist = []
+
+for i in xlist:
+  hlist.append(h(i))
+
+print(xlist)
+print(hlist)
